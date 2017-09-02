6b1142bf5144c9b65ca99da40a2464cbcfabe031
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:36:28 +0100

    Initial import

---
+++ TargaMS.h
@@ -0,0 +1,29 @@
+#ifndef _TARGAMS_H
+#define _TARGAMS_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t TargaMS10[];
+    extern const uint8_t TargaMS11[];
+    extern const uint8_t TargaMS12[];
+    extern const uint8_t TargaMS13[];
+    extern const uint8_t TargaMS14[];
+    extern const uint8_t TargaMS15[];
+    extern const uint8_t TargaMS16[];
+    extern const uint8_t TargaMS17[];
+    extern const uint8_t TargaMS18[];
+    extern const uint8_t TargaMS19[];
+    extern const uint8_t TargaMS20[];
+    extern const uint8_t TargaMS21[];
+    extern const uint8_t TargaMS22[];
+    extern const uint8_t TargaMS23[];
+    extern const uint8_t TargaMS24[];
+    extern const uint8_t TargaMS25[];
+    extern const uint8_t TargaMS26[];
+    extern const uint8_t TargaMS27[];
+    extern const uint8_t TargaMS28[];
+    extern const uint8_t TargaMS29[];
+    extern const uint8_t TargaMS30[];
+};
+#endif
